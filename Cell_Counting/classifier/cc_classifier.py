59e83381f658aad9999814872741072789c09458
gm515 <gm515@imperial.ac.uk>
2018-10-10 16:49:00 +0100

    finally gpu works

--- Cell_Counting/classifier/cc_classifier.py
+++ Cell_Counting/classifier/cc_classifier.py
@@ -6,6 +6,7 @@ import shutil
 import datetime
 from multiprocessing import cpu_count
 
+import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
