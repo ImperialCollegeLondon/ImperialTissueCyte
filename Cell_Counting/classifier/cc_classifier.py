4573dd29387907fbd4c73886a7502d35693253b2
gm515 <gm515@imperial.ac.uk>
2018-10-10 16:39:36 +0100

    Added GPU support for classifier

--- Cell_Counting/classifier/cc_classifier.py
+++ Cell_Counting/classifier/cc_classifier.py
@@ -11,6 +11,9 @@ os.environ['GOTO_NUM_THREADS'] = str(cpu_count())
 os.environ['OMP_NUM_THREADS'] = str(cpu_count())
 os.environ['openmp'] = 'True'
 
+config = tf.ConfigProto(device_count={"GPU" : 1, "CPU" : cpu_count()})
+keras.backend.tensorflow_backend.set_session(tf.Session(config=config))
+
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
