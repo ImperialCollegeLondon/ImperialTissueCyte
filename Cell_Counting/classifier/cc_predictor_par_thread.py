e2d1aa5cffedecfcd66a1c9742afad7dd9d704a6
gm515 <gm515@imperial.ac.uk>
2018-10-15 14:46:30 +0100

    Message

--- Cell_Counting/classifier/cc_predictor_par_thread.py
+++ Cell_Counting/classifier/cc_predictor_par_thread.py
@@ -146,6 +146,8 @@ if __name__ == '__main__':
 
     pool = Pool(cpu_count())
 
+    res = pool.apply_async(partial(cellpredict, model_path=model_path, marker=marker, image_path=image_path, filename=filename, cell_markers=cell_markers, nocell_markers=nocell_markers), cell_index), 1)
+
     # for i, _ in enumerate(pool.apply_async(partial(cellpredict, model_path=model_path, marker=marker, image_path=image_path, filename=filename, cell_markers=cell_markers, nocell_markers=nocell_markers), cell_index), 1):
     #     sys.stderr.write('\rDone {0:%}'.format(i/len(cell_index)))
 
