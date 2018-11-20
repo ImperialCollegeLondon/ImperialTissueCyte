be8c199a1fbb2fc0878690f5a614a315ada90d67
gm515 <gm515@imperial.ac.uk>
2018-11-20 13:29:29 +0000

    Refined training data and added new classifier

--- Cell_Counting/classifier/cc_classifier_2.py
+++ Cell_Counting/classifier/cc_classifier_2.py
@@ -16,7 +16,7 @@ import skimage.exposure
 import keras
 from keras.models import Sequential
 from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
+from keras.layers import Activation, Dropout, Flatten, Dense, BatchNormalization
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import RMSprop
 import tensorflow as tf
