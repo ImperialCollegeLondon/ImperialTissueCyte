405e1f53ee421ae99fd9c472666020011fa95498
gm515 <gm515@imperial.ac.uk>
2019-01-30 15:25:01 +0000

    new model

--- cell_counting/classifier/cc_classifier_aug_2.py
+++ cell_counting/classifier/cc_classifier_aug_2.py
@@ -7,9 +7,10 @@ import skimage.exposure
 import Augmentor
 import tensorflow as tf
 
+import keras
 from keras.models import Sequential, load_model
-from keras.layers.core import Dropout, Flatten, Dense
-from keras.layers.convolution import Conv2D, MaxPooling2D, ZeroPadding2D
+from keras.layers import Dropout, Flatten, Dense
+from keras.layers import Conv2D, MaxPooling2D, ZeroPadding2D
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import RMSprop
 
@@ -43,39 +44,39 @@ print "Constructing Convolution Neural Network..."
 
 model = Sequential()
 model.add(ZeroPadding2D((1,1),input_shape=(80, 80, 1)))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(64, (3, 3), activation='relu'))
+model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(128, (3, 3), activation='relu'))
+model.add(Conv2D(128, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(Conv2D(256, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(Conv2D(256, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(256, (3, 3), activation='relu'))
+model.add(Conv2D(256, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(Conv2D(512, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(Conv2D(512, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(Conv2D(512, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(Conv2D(512, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(Conv2D(512, (3, 3), activation='relu'))
 model.add(ZeroPadding2D((1,1)))
-model.add(Convolution2D(512, (3, 3), activation='relu'))
+model.add(Conv2D(512, (3, 3), activation='relu'))
 model.add(MaxPooling2D((2,2), strides=(2,2)))
 
 model.add(Flatten())
