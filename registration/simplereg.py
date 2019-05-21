42144138496a39771259b1dc7af2d51a8b25a19a
gm515 <gm515@imperial.ac.uk>
2019-05-21 14:31:34 +0100

    Final simple

--- registration/simplereg.py
+++ registration/simplereg.py
@@ -3,18 +3,38 @@ import time
 import argparse
 from skimage import io
 
+def slack_message(text, channel, username):
+    from urllib import request, parse
+    import json
+
+    post = {"text": "{0}".format(text),
+        "channel": "{0}".format(channel),
+        "username": "{0}".format(username),
+        "icon_url": "https://github.com/gm515/gm515.github.io/blob/master/Images/imperialstplogo.png?raw=true"}
+
+    try:
+        json_data = json.dumps(post)
+        req = request.Request('https://hooks.slack.com/services/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',
+            data=json_data.encode('ascii'),
+            headers={'Content-Type': 'application/json'})
+        resp = request.urlopen(req)
+    except Exception as em:
+        print("EXCEPTION: " + str(em))
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('autoflpath', default=[], type=str, help='File path for autofluorescence atlas')
     parser.add_argument('outpath', default=[], type=str, help='Directory path to save output files')
-    parser.add_argument('-avgpath', default='average_10um.tif', type=str, help='File path for average atlas')
-    parser.add_argument('-annopath', default='annotation_10um.tif', type=str, help='File path for annotation atlas')
-    parser.add_argument('-hempath', default='hemisphere_10um.tif', type=str, help='File path for hemisphere atlas')
+    parser.add_argument('-avgpath', default='atlases/average_10um.tif', type=str, help='File path for average atlas')
+    parser.add_argument('-annopath', default='atlases/annotation_10um.tif', type=str, help='File path for annotation atlas')
+    parser.add_argument('-hempath', default='atlases/hemisphere_10um.tif', type=str, help='File path for hemisphere atlas')
     parser.add_argument('-first', default=1, type=int, dest='first', help='First slice in average atlas')
     parser.add_argument('-last', default=1140, type=int, dest='last', help='Last slice in average atlas')
 
     args = parser.parse_args()
+
+try:
 
     print ('Loading all atlases...')
     fixedData = sitk.ReadImage(args.autoflpath)
@@ -64,3 +84,16 @@ if __name__ == '__main__':
     # Write average transform and segmented results
     sitk.WriteImage(resultSeg, args.outpath+'/segres_10um.tif')
     sitk.WriteImage(hemSeg, args.outpath+'/hemres_10um.tif')
+
+except (RuntimeError, TypeError, NameError, ImportError, SyntaxError):
+    slack_message('SimpleElastix segmentation ERROR', '#segmentation', 'Segmentation')
+
+minutes, seconds = divmod(time.time()-tstart, 60)
+hours, minutes = divmod(minutes, 60)
+days, hours = divmod(hours, 24)
+text = 'SimpleElastix segmentation completed in %02d:%02d:%02d:%02d' %(days, hours, minutes, seconds)
+
+print ('')
+print (text)
+
+slack_message(text, '#segmentation', 'Segmentation')
