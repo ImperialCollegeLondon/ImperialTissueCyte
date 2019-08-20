7f7cbbc166c29a9746a7178f8f2210d503fb9dcf
gm515 <gm515@imperial.ac.uk>
2019-08-20 16:53:33 +0100

    ddd

--- cell_counting/classifier/preprocessing.py
+++ cell_counting/classifier/preprocessing.py
@@ -158,12 +158,12 @@ def preprocess(normalise):
 
         for idx, img in enumerate(training_data_all):
             if (np.max(img)-np.min(img)) == 0:
-                print ('Min: {0:.2f} Max: {1:.2f}'.format(np.min(img), np.max(max)))
+                print ('Min: {0:.2f} Max: {1:.2f}'.format(np.min(img), np.max(img)))
             training_data_all[idx] = (img-np.min(img))/(np.max(img)-np.min(img))
             # print ('Min: {0:.2f} Max: {1:.2f}'.format(np.min(training_data_all[idx]), np.max(training_data_all[idx])))
         for idx, img in enumerate(test_data_all):
             if (np.max(img)-np.min(img)) == 0:
-                print ('Min: {0:.2f} Max: {1:.2f}'.format(np.min(img), np.max(max)))
+                print ('Min: {0:.2f} Max: {1:.2f}'.format(np.min(img), np.max(img)))
             test_data_all[idx] = (img-np.min(img))/(np.max(img)-np.min(img))
             # print ('Min: {0:.2f} Max: {1:.2f}'.format(np.min(test_data_all[idx]), np.max(test_data_all[idx])))
 
