8056ccef6d81dbc4305c39ea7d82a14457c0be6c
gm515 <gm515@imperial.ac.uk>
2019-09-04 11:55:56 +0100

    drop out to 0.5

--- neuroseg/unetmodel.py
+++ neuroseg/unetmodel.py
@@ -14,91 +14,91 @@ def focal_loss(gamma=2., alpha=.25):
 
 
 def get_unet(do=0, activation=ReLU):
-    inputs = Input(shape = (None, None, 1))
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
-    conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    drop4 = Dropout(0.1)(conv4)
-    pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
-
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
-    conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    drop5 = Dropout(0.1)(conv5)
-
-    up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
-    merge6 = concatenate([drop4,up6], axis = 3)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
-    conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
-
-    up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
-    merge7 = concatenate([conv3,up7], axis = 3)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
-    conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
-
-    up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
-    merge8 = concatenate([conv2,up8], axis = 3)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
-    conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
-
-    up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
-    merge9 = concatenate([conv1,up9], axis = 3)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
-    conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
-
-    model = Model(input = inputs, output = conv10)
-
-    model.compile(optimizer = Adam(lr = 1e-4), loss = focal_loss(gamma=2., alpha=.25), metrics = ['accuracy'])
     # inputs = Input(shape = (None, None, 1))
-    # conv1 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(inputs)))
-    # conv1 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(conv1)))
+    # conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
+    # conv1 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
     # pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
-    #
-    # conv2 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(pool1)))
-    # conv2 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(conv2)))
+    # conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
+    # conv2 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
     # pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
-    #
-    # conv3 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(pool2)))
-    # conv3 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(conv3)))
+    # conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
+    # conv3 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
     # pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+    # conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
+    # conv4 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
+    # drop4 = Dropout(0.1)(conv4)
+    # pool4 = MaxPooling2D(pool_size=(2, 2))(drop4)
     #
-    # conv4 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(pool3)))
-    # conv4 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(conv4)))
-    # pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+    # conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
+    # conv5 = Conv2D(1024, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
+    # drop5 = Dropout(0.1)(conv5)
     #
-    # conv5 = Dropout(do)(activation()(Conv2D(512, (3, 3), padding='same')(pool4)))
-    # conv5 = Dropout(do)(activation()(Conv2D(512, (3, 3), padding='same')(conv5)))
+    # up6 = Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(drop5))
+    # merge6 = concatenate([drop4,up6], axis = 3)
+    # conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
+    # conv6 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
     #
-    # up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
-    # conv6 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(up6)))
-    # conv6 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(conv6)))
+    # up7 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv6))
+    # merge7 = concatenate([conv3,up7], axis = 3)
+    # conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
+    # conv7 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
     #
-    # up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
-    # conv7 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(up7)))
-    # conv7 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(conv7)))
+    # up8 = Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv7))
+    # merge8 = concatenate([conv2,up8], axis = 3)
+    # conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
+    # conv8 = Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
     #
-    # up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
-    # conv8 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(up8)))
-    # conv8 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(conv8)))
+    # up9 = Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv8))
+    # merge9 = concatenate([conv1,up9], axis = 3)
+    # conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
+    # conv9 = Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    # conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
+    # conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
     #
-    # up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
-    # conv9 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(up9)))
-    # conv9 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(conv9)))
+    # model = Model(input = inputs, output = conv10)
     #
-    # conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
-    #
-    # model = Model(inputs=[inputs], outputs=[conv10])
-    #
-    # model.compile(optimizer=Adam(lr=1e-5), loss=focal_loss(gamma=2., alpha=.25), metrics=['accuracy'])
+    # model.compile(optimizer = Adam(lr = 1e-4), loss = focal_loss(gamma=2., alpha=.25), metrics = ['accuracy'])
+    inputs = Input(shape = (None, None, 1))
+    conv1 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(inputs)))
+    conv1 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(conv1)))
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1)
+    
+    conv2 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(pool1)))
+    conv2 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(conv2)))
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2)
+
+    conv3 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(pool2)))
+    conv3 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(conv3)))
+    pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
+
+    conv4 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(pool3)))
+    conv4 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(conv4)))
+    pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
+
+    conv5 = Dropout(do)(activation()(Conv2D(512, (3, 3), padding='same')(pool4)))
+    conv5 = Dropout(do)(activation()(Conv2D(512, (3, 3), padding='same')(conv5)))
+
+    up6 = concatenate([Conv2DTranspose(256, (2, 2), strides=(2, 2), padding='same')(conv5), conv4], axis=3)
+    conv6 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(up6)))
+    conv6 = Dropout(do)(activation()(Conv2D(256, (3, 3), padding='same')(conv6)))
+
+    up7 = concatenate([Conv2DTranspose(128, (2, 2), strides=(2, 2), padding='same')(conv6), conv3], axis=3)
+    conv7 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(up7)))
+    conv7 = Dropout(do)(activation()(Conv2D(128, (3, 3), padding='same')(conv7)))
+
+    up8 = concatenate([Conv2DTranspose(64, (2, 2), strides=(2, 2), padding='same')(conv7), conv2], axis=3)
+    conv8 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(up8)))
+    conv8 = Dropout(do)(activation()(Conv2D(64, (3, 3), padding='same')(conv8)))
+
+    up9 = concatenate([Conv2DTranspose(32, (2, 2), strides=(2, 2), padding='same')(conv8), conv1], axis=3)
+    conv9 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(up9)))
+    conv9 = Dropout(do)(activation()(Conv2D(32, (3, 3), padding='same')(conv9)))
+
+    conv10 = Conv2D(1, (1, 1), activation='sigmoid')(conv9)
+
+    model = Model(inputs=[inputs], outputs=[conv10])
+
+    model.compile(optimizer=Adam(lr=1e-5), loss=focal_loss(gamma=2., alpha=.25), metrics=['accuracy'])
 
 
     return model
