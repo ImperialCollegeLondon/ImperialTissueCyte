4fa3e86def6758027def1b6721a8e77dc32de993
gm515 <gm515@imperial.ac.uk>
2019-10-01 15:38:33 +0100

    test

--- neuroseg/predictor.py
+++ neuroseg/predictor.py
@@ -68,8 +68,8 @@ if __name__ == '__main__':
     # weights_path = args.weightspath
 
 
-    model_path = '/Users/gm515/Desktop/_works_anysize_2019_09_30_UNet/focal_unet_model.json'
-    weights_path = '/Users/gm515/Desktop/_works_anysize_2019_09_30_UNet/focal_unet_weights.best.hdf5'
+    model_path = 'models/2019_09_30_UNet/focal_unet_model.json'
+    weights_path = 'models/2019_09_30_UNet/focal_unet_weights.best.hdf5'
 
     # Load the classifier model, initialise and compile
     with open(model_path, 'r') as f:
@@ -83,7 +83,7 @@ if __name__ == '__main__':
 
     images_array = []
 
-    img = np.array(Image.open('/Users/gm515/Desktop/unet-test/00007.tif')).astype(np.float32)
+    img = np.array(Image.open('00007.tif')).astype(np.float32)
     img = (img-np.min(img))/(np.max(img)-np.min(img))
     img_copy = np.copy(img)
 
