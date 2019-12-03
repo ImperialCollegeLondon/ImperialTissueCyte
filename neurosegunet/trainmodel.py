c3e79e9135c4e4464eabec664e5331c5db511c02
gm515 <gm515@imperial.ac.uk>
2019-12-03 18:43:55 +0000

    d

--- neurosegunet/trainmodel.py
+++ neurosegunet/trainmodel.py
@@ -47,8 +47,8 @@ if __name__ == '__main__':
     batch = 4
 
     # ORIG BATCH NORM LOSS
-    # model = unetmodel.get_unet(losses.binary_focal_loss(alpha=.25, gamma=2))
-    model = unetmodel.get_unet('binary_crossentropy')
+    model = unetmodel.get_unet(losses.binary_focal_loss(alpha=.25, gamma=2))
+    # model = unetmodel.get_unet('binary_crossentropy')
     # model = unetmodel.get_unet(losses.focal_loss)
 
     checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
