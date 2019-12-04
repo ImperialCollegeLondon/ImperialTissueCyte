b53ee78a837bec6161065c437e1f32224ae4dfb7
gm515 <gm515@imperial.ac.uk>
2019-12-04 13:14:56 +0000

    tversky loss

--- neurosegunet/trainmodel.py
+++ neurosegunet/trainmodel.py
@@ -47,9 +47,9 @@ if __name__ == '__main__':
     batch = 4
 
     # ORIG BATCH NORM LOSS
-    model = unetmodel.get_unet(losses.binary_focal_loss(alpha=.25, gamma=2))
+    # model = unetmodel.get_unet(losses.binary_focal_loss(alpha=.25, gamma=2))
     # model = unetmodel.get_unet('binary_crossentropy')
-    # model = unetmodel.get_unet(losses.focal_loss)
+    model = unetmodel.get_unet(losses.focal_tversky)
 
     checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
     early = EarlyStopping(monitor="val_loss", mode="min", patience=50, verbose=1)
