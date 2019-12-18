844d0397aae01f09013451fea9c38100a7b42800
gm515 <gm515@imperial.ac.uk>
2019-12-18 12:04:22 +0000

    dice to val

--- cell_counting_unet/trainmodel.py
+++ cell_counting_unet/trainmodel.py
@@ -54,9 +54,9 @@ if __name__ == '__main__':
     model = unetmodel.get_unet(losses.focal_tversky)
     # model = unetmodel.get_unet(losses.bce_dice_loss)
 
-    checkpoint = ModelCheckpoint(file_path, monitor='val_dice_loss', verbose=1, save_best_only=True, mode='min')
-    early = EarlyStopping(monitor="val_dice_loss", mode="min", patience=50, verbose=1)
-    redonplat = ReduceLROnPlateau(monitor="val_dice_loss", mode="min", patience=20, verbose=1)
+    checkpoint = ModelCheckpoint(file_path, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+    early = EarlyStopping(monitor="val_loss", mode="min", patience=50, verbose=1)
+    redonplat = ReduceLROnPlateau(monitor="val_loss", mode="min", patience=20, verbose=1)
     callbacks_list = [checkpoint, early, redonplat]  # early
 
     history = model.fit(train_x, train_y,
