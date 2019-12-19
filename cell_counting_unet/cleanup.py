7074bc8b3fa7c57e6574b173e7afdfc5ddf2b336
gm515 <gm515@imperial.ac.uk>
2019-12-19 17:10:43 +0000

    d

--- cell_counting_unet/cleanup.py
+++ cell_counting_unet/cleanup.py
@@ -16,10 +16,12 @@ import shutil
 def clean():
     training_data_dir = 'input/training_data'
     test_data_dir = 'input/test_data'
+    raw_data_copy_dir = 'input/raw-data-copy'
 
     print ('Cleaning up directories...')
 
     if os.path.exists(training_data_dir) and os.path.isdir(training_data_dir): shutil.rmtree(training_data_dir)
     if os.path.exists(test_data_dir) and os.path.isdir(test_data_dir): shutil.rmtree(test_data_dir)
+    if os.path.exists(raw_data_copy_dir) and os.path.isdir(raw_data_copy_dir): shutil.rmtree(raw_data_copy_dir)
 
     print ('Done!')
