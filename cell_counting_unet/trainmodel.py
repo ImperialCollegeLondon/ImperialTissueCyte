6a0f4dfa8bd566794d6e8da905b481f76f3a53d6
gm515 <gm515@imperial.ac.uk>
2019-12-20 14:19:13 +0000

    b

--- cell_counting_unet/trainmodel.py
+++ cell_counting_unet/trainmodel.py
@@ -50,9 +50,9 @@ if __name__ == '__main__':
 
     # ORIG BATCH NORM LOSS
     # model = unetmodel.get_unet(losses.binary_focal_loss)
-    model = unetmodel.get_unet('binary_crossentropy')
+    # model = unetmodel.get_unet('binary_crossentropy')
     # model = unetmodel.get_unet(losses.focal_tversky)
-    # model = unetmodel.get_unet(losses.bce_dice_loss)
+    model = unetmodel.get_unet(losses.bce_dice_loss)
     # model = unetmodel.get_unet(losses.focal_loss)
 
     checkpoint = ModelCheckpoint(file_path, monitor='val_dice_loss', verbose=1, save_best_only=True, mode='min')
