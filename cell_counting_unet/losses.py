814835418b13fd3b485878f46300a3a4bb19a60a
gm515 <gm515@imperial.ac.uk>
2019-12-20 12:13:53 +0000

    uy

--- cell_counting_unet/losses.py
+++ cell_counting_unet/losses.py
@@ -33,7 +33,9 @@ def focal_loss(y_true, y_pred):
     gamma=2
     pt_1 = tf.where(tf.equal(y_true, 1), y_pred, tf.ones_like(y_pred))
     pt_0 = tf.where(tf.equal(y_true, 0), y_pred, tf.zeros_like(y_pred))
-    return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+
+    return -K.sum(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.sum((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
+    # return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 #
 # def focal_loss(alpha=0.25, gamma=2):
 #     def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
