cdcbcd567226c1f83199079e55bede800796edf4
gm515 <gm515@imperial.ac.uk>
2019-12-20 12:00:17 +0000

    rw

--- cell_counting_unet/losses.py
+++ cell_counting_unet/losses.py
@@ -36,22 +36,22 @@ def binary_focal_loss(y_true, y_pred):
 #     return -K.mean(alpha * K.pow(1. - pt_1, gamma) * K.log(pt_1)) - K.mean((1 - alpha) * K.pow(pt_0, gamma) * K.log(1. - pt_0))
 
 def focal_loss(alpha=0.25, gamma=2):
-  def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
-    weight_a = alpha * (1 - y_pred) ** gamma * targets
-    weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
+    def focal_loss_with_logits(logits, targets, alpha, gamma, y_pred):
+        weight_a = alpha * (1 - y_pred) ** gamma * targets
+        weight_b = (1 - alpha) * y_pred ** gamma * (1 - targets)
 
-    return (tf.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b
+        return (tf.log1p(tf.exp(-tf.abs(logits))) + tf.nn.relu(-logits)) * (weight_a + weight_b) + logits * weight_b
 
-  def loss(y_true, y_pred):
-    y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
-    logits = tf.log(y_pred / (1 - y_pred))
+    def loss(y_true, y_pred):
+        y_pred = tf.clip_by_value(y_pred, K.epsilon(), 1 - K.epsilon())
+        logits = tf.log(y_pred / (1 - y_pred))
 
-    loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
+        loss = focal_loss_with_logits(logits=logits, targets=y_true, alpha=alpha, gamma=gamma, y_pred=y_pred)
 
-    # or reduce_sum and/or axis=-1
-    return tf.reduce_mean(loss)
+        # or reduce_sum and/or axis=-1
+        return tf.reduce_mean(loss)
 
-  return loss
+    return loss
 
 def dsc(y_true, y_pred):
     smooth = 1.
