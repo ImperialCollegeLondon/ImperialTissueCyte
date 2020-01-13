f79e7824c0a550b680de4e4b0b9fa86f4afbe23e
gm515 <gm515@imperial.ac.uk>
2020-01-13 14:15:51 +0000

    weighted cros entropy

--- cell_counting_unet/losses.py
+++ cell_counting_unet/losses.py
@@ -5,6 +5,22 @@ import tensorflow as tf
 epsilon = 1e-5
 smooth = 1
 
+def weighted_cross_entropy(beta):
+  def convert_to_logits(y_pred):
+      # see https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/python/keras/backend.py#L3525
+      y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
+
+      return tf.log(y_pred / (1 - y_pred))
+
+  def loss(y_true, y_pred):
+    y_pred = convert_to_logits(y_pred)
+    loss = tf.nn.weighted_cross_entropy_with_logits(logits=y_pred, targets=y_true, pos_weight=beta)
+
+    # or reduce_sum and/or axis=-1
+    return tf.reduce_mean(loss)
+
+  return loss
+
 def binary_focal_loss(y_true, y_pred):
     """
     Binary form of focal loss.
