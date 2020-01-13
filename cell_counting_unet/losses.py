cb88abd4bb1d8ca854bd35f4ba2ee16a29a61501
gm515 <gm515@imperial.ac.uk>
2020-01-13 14:22:14 +0000

    tf versin co

--- cell_counting_unet/losses.py
+++ cell_counting_unet/losses.py
@@ -10,7 +10,7 @@ def weighted_cross_entropy(beta):
       # see https://github.com/tensorflow/tensorflow/blob/r1.10/tensorflow/python/keras/backend.py#L3525
       y_pred = tf.clip_by_value(y_pred, tf.keras.backend.epsilon(), 1 - tf.keras.backend.epsilon())
 
-      return tf.log(y_pred / (1 - y_pred))
+      return tf.math.log(y_pred / (1 - y_pred))
 
   def loss(y_true, y_pred):
     y_pred = convert_to_logits(y_pred)
