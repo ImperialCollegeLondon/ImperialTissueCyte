468db04ebc9ec7e84c2df80948e5403a9a6045e0
gm515 <gm515@imperial.ac.uk>
2020-01-15 12:30:32 +0000

    s

--- cell_counting_unet/trainmodel.py
+++ cell_counting_unet/trainmodel.py
@@ -65,7 +65,7 @@ if __name__ == '__main__':
     history = model.fit(train_x, train_y,
         validation_data=(val_x, val_y),
         batch_size=batch,
-        epochs=2000,
+        epochs=1500,
         shuffle=True,
         callbacks=callbacks_list)
 
