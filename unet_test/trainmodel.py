e78d76a9e4451d5a9a85b119e4d49d21efe85e02
gm515 <gm515@imperial.ac.uk>
2020-01-21 15:28:36 +0000

    b

--- unet_test/trainmodel.py
+++ unet_test/trainmodel.py
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     history = model.fit(train_x, train_y,
         validation_data=(val_x, val_y),
         batch_size=batch,
-        epochs=500,
+        epochs=100,
         shuffle=True,
         callbacks=callbacks_list)
 
