5fd363bd5131bc5de9915213ca487a2ac60b6eaa
gm515 <gm515@imperial.ac.uk>
2020-01-22 14:48:17 +0000

    g

--- unet_test/trainmodel.py
+++ unet_test/trainmodel.py
@@ -57,12 +57,12 @@ if __name__ == '__main__':
     batch = 4
 
     # ORIG BATCH NORM LOSS
-    # model = unetmodel.get_unet('binary_crossentropy')
+    model = unetmodel.get_unet('binary_crossentropy')
     # model = unetmodel.get_unet(losses.binary_focal_loss)
     # model = unetmodel.get_unet(losses.focal_tversky)
     # model = unetmodel.get_unet(losses.bce_dice_loss)
     # model = unetmodel.get_unet(losses.focal_loss)
-    model = unetmodel.get_unet(losses.weighted_cross_entropy(0.75))
+    # model = unetmodel.get_unet(losses.weighted_cross_entropy(0.75))
     # model = unetmodel.get_unet(losses.bce_focal_tversky_loss(alpha))
     # model = unetmodel.get_unet(losses.surface_loss)
     # model = unetmodel.get_unet(losses.dice_focal_tversky_loss(alpha))
@@ -72,8 +72,8 @@ if __name__ == '__main__':
     # model = unetmodel.get_unet(losses.iou)
 
     checkpoint = ModelCheckpoint(file_path, monitor='val_dice_loss', verbose=1, save_best_only=True, mode='min')
-    early = EarlyStopping(monitor="val_loss", mode="min", patience=50, verbose=1)
-    redonplat = ReduceLROnPlateau(monitor="val_loss", mode="min", patience=20, verbose=1)
+    early = EarlyStopping(monitor="val_dice_loss", mode="min", patience=50, verbose=1)
+    redonplat = ReduceLROnPlateau(monitor="val_dice_loss", mode="min", patience=20, verbose=1)
     newalpha = AlphaCallback(alpha)
     callbacks_list = [checkpoint, early, redonplat, newalpha]
 
