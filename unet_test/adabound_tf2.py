a8ce8e6e4d1c52cd600adf4379956bdea11537ef
gm515 <gm515@imperial.ac.uk>
2020-02-24 18:18:42 +0000

    swtiched back

--- unet_test/adabound_tf2.py
+++ unet_test/adabound_tf2.py
@@ -6,7 +6,7 @@ from __future__ import print_function
 import tensorflow as tf
 import re
 
-class AdaBoundOptimizer(tf.keras.optimizers.Optimizer):
+class AdaBoundOptimizer(tf.compat.v1.train.Optimizer):
     """Optimizer that implements the AdaBound algorithm.
     See [Luo et al., 2019](https://openreview.net/forum?id=Bkg3g2R9FX)
     ([pdf](https://openreview.net/pdf?id=Bkg3g2R9FX)).
