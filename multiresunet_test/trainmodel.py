5dfc3f1907c5edcfb244a22759ee0ce2459f8c65
gm515 <gm515@imperial.ac.uk>
2020-02-27 15:33:08 +0000

    changed epochs and batch

--- multiresunet_test/trainmodel.py
+++ multiresunet_test/trainmodel.py
@@ -54,7 +54,7 @@ if __name__ == '__main__':
 
     filepath = os.path.join(savedirpath, modelname+'weights.best.hdf5')
 
-    batch = 4
+    batch = 6
 
     # Loss functions for training
     model = multiresunetmodel.multiresunet(input_size=(None, None, 1), opt_fn=optimizer, loss_fn='binary_crossentropy')
@@ -80,7 +80,7 @@ if __name__ == '__main__':
     history = model.fit(train_x, train_y,
         validation_data=(val_x, val_y),
         batch_size=batch,
-        epochs=150,
+        epochs=250,
         shuffle=True,
         callbacks=callbacks_list)
 
