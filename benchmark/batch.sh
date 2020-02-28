e0900938fee1a8097aff4d47ad73454a7fc3bc13
gm515 <gm515@imperial.ac.uk>
2020-02-28 15:13:57 +0000

    g

--- benchmark/batch.sh
+++ benchmark/batch.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-ipython benchmark.py -- 2020_02_25_Adam_lr0.0001_MultiResUNet
-ipython benchmark.py -- 2020_02_26_SGD_lr0.0001_MultiResUNet
-ipython benchmark.py -- 2020_02_26_SGD_lr0.1_MultiResUNet
-ipython benchmark.py -- 2020_02_27_SGD_lr0.5_MultiResUNet
-ipython benchmark.py -- 2020_02_26_Adam_lr0.0001_MultiResUNet
-ipython benchmark.py -- 2020_02_26_SGD_lr0.01_MultiResUNet
-ipython benchmark.py -- 2020_02_27_Adam_lr0.0001_MultiResUNet
+ipython benchmark.py -- ../multiresunet_test/2020_02_25_Adam_lr0.0001_MultiResUNet
+ipython benchmark.py -- ../multiresunet_test/2020_02_26_SGD_lr0.0001_MultiResUNet
+ipython benchmark.py -- ../multiresunet_test/2020_02_26_SGD_lr0.1_MultiResUNet
+ipython benchmark.py -- ../multiresunet_test/2020_02_27_SGD_lr0.5_MultiResUNet
+ipython benchmark.py -- ../multiresunet_test/2020_02_26_Adam_lr0.0001_MultiResUNet
+ipython benchmark.py -- ../multiresunet_test/2020_02_26_SGD_lr0.01_MultiResUNet
+ipython benchmark.py -- ../multiresunet_test/2020_02_27_Adam_lr0.0001_MultiResUNet
