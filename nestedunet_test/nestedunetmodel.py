d957ac3fcf46768ca81a65873f11f23c1396cf96
gm515 <gm515@imperial.ac.uk>
2020-03-05 14:47:36 +0000

    b

--- nestedunet_test/nestedunetmodel.py
+++ nestedunet_test/nestedunetmodel.py
@@ -21,6 +21,48 @@ def standard_unit(input_tensor, stage, nb_filter, kernel_size=3):
 
     return x
 
+def unet(optfn, lossfn, inputsize=(None, None, 1)):
+    img_input = Input(inputsize)
+    
+    conv1_1 = standard_unit(img_input, stage='11', nb_filter=32)
+    pool1 = MaxPooling2D((2, 2), strides=(2, 2), name='pool1')(conv1_1)
+
+    conv2_1 = standard_unit(pool1, stage='21', nb_filter=64)
+    pool2 = MaxPooling2D((2, 2), strides=(2, 2), name='pool2')(conv2_1)
+
+    conv3_1 = standard_unit(pool2, stage='31', nb_filter=128)
+    pool3 = MaxPooling2D((2, 2), strides=(2, 2), name='pool3')(conv3_1)
+
+    conv4_1 = standard_unit(pool3, stage='41', nb_filter=256)
+    pool4 = MaxPooling2D((2, 2), strides=(2, 2), name='pool4')(conv4_1)
+
+    conv5_1 = standard_unit(pool4, stage='51', nb_filter=512)
+
+    up4_2 = Conv2DTranspose(256, (2, 2), strides=(2, 2), name='up42', padding='same')(conv5_1)
+    conv4_2 = concatenate([up4_2, conv4_1], name='merge42', axis=3)
+    conv4_2 = standard_unit(conv4_2, stage='42', nb_filter=256)
+
+    up3_3 = Conv2DTranspose(128, (2, 2), strides=(2, 2), name='up33', padding='same')(conv4_2)
+    conv3_3 = concatenate([up3_3, conv3_1], name='merge33', axis=3)
+    conv3_3 = standard_unit(conv3_3, stage='33', nb_filter=128)
+
+    up2_4 = Conv2DTranspose(64, (2, 2), strides=(2, 2), name='up24', padding='same')(conv3_3)
+    conv2_4 = concatenate([up2_4, conv2_1], name='merge24', axis=3)
+    conv2_4 = standard_unit(conv2_4, stage='24', nb_filter=64)
+
+    up1_5 = Conv2DTranspose(32, (2, 2), strides=(2, 2), name='up15', padding='same')(conv2_4)
+    conv1_5 = concatenate([up1_5, conv1_1], name='merge15', axis=3)
+    conv1_5 = standard_unit(conv1_5, stage='15', nb_filter=32)
+
+    unet_output = Conv2D(num_class, (1, 1), activation='sigmoid', name='output', kernel_initializer = 'he_normal', padding='same')(conv1_5)
+
+    model = Model(input=img_input, output=unet_output)
+
+    model.compile(optimizer=optfn, loss=lossfn, metrics=[losses.dice_loss, lossfn])
+
+    return model
+
+
 def nestedunet(optfn, lossfn, inputsize=(None, None, 1), deep_supervision=False):
     img_input = Input(inputsize)
 
